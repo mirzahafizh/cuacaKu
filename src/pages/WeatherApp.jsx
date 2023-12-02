32703891bfca03b2c6ae7e6063cce145b552bb95
mirzahaifzh <mirzafio78@gmail.com>
2023-12-03 01:18:30 +0800

    fix

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -4,10 +4,8 @@ import { default as React, useEffect, useState } from 'react';
 
 import KontenDua from '../components/KontenDua';
 import KontenEmpat from '../components/KontenEmpat';
-import KontenLima from '../components/KontenLima';
 import KontenSatu from '../components/KontenSatu';
 import KontenSix from '../components/KontenSix';
-import KontenTiga from '../components/KontenTiga';
 import SearchBar from '../components/SearchBar';
 import ToggleButton from '../components/ToggleButton';
 import dummyData from '../data/dummyData.json';
@@ -116,20 +114,28 @@ const WeatherApp = ({darkMode,setDarkMode}) => {
 
 
     return (
-        <div className={`container mx-auto p-6 w-auto ${darkMode ?'bg-gradient-to-r from-neutral-700 to-stone-900 ':'bg-gradient-to-br from-white to-slate-600'}`}>
-        <div className="flex justify-between items-center">
-            <h1 className={`text-[35px]  font-['Poppins'] ${darkMode ?' text-white ':' text-black' }`}>CuacaKu</h1>
-            <SearchBar handleInputChange={handleInputChange} handleKeyPress={handleKeyPress} handleSearch={handleSearch} searchTerm={searchTerm}/>
-            <ToggleButton darkMode={darkMode} setDarkMode={setDarkMode}/>
-        </div>
+        <div className={`border-4 container mx-auto p-6 w-auto ${darkMode ? 'bg-gradient-to-r from-neutral-700 to-stone-900 ' : 'bg-gradient-to-br from-white to-slate-600'}`}>
+            <div className="flex flex-col max-w-5xl mx-auto ">
+                <div className="flex justify-between md:flex-row gap-4">
+                <h1 className={`text-[50px]  text-[Poppins] text-center mb-4 md:mb-0 ${darkMode ? 'text-white' : 'text-black'}`}>CuacaKu</h1>
+                    <SearchBar handleInputChange={handleInputChange} handleKeyPress={handleKeyPress} handleSearch={handleSearch} searchTerm={searchTerm} />
+                    <ToggleButton darkMode={darkMode} setDarkMode={setDarkMode} />
+                </div>
+            </div>
         <KontenSatu getWeatherIcon={getWeatherIcon} darkMode={darkMode} currentWeather={currentWeather}/>
-
         <KontenDua getWeatherIcon={getWeatherIcon} darkMode={darkMode} currentWeather={currentWeather} fiveDaysForecast={fiveDaysForecast} hourlyForecast={hourlyForecast}/>
-        <KontenTiga currentWeather={currentWeather}/>
+
+            <div className={`rounded-[11px]  text-center mx-auto text-white font-bold text-xl p-1 lg:max-w-5xl lg:w-72 w-full${darkMode ? ' bg-neutral-600 ' : ' bg-zinc-400'}`}>
+                Prakiraan Cuaca
+            </div>
         <KontenEmpat getWeatherIcon={getWeatherIcon} currentWeather={currentWeather} darkMode={darkMode} setCurrentWeather={setCurrentWeather} fiveDaysForecast={fiveDaysForecast} hourlyForecast={hourlyForecast}/>
-        <KontenLima/>
-        <KontenSix currentWeather={currentWeather} darkMode={darkMode}/>
+
+            <div className={`rounded-[11px] mt-4 text-center mx-auto text-white font-bold text-xl p-1 lg:max-w-5xl lg:w-72 w-full${darkMode ? ' bg-neutral-600 ' : ' bg-zinc-400'}`}>
+                Kesehatan & Keselamatan
+            </div>
+            <KontenSix currentWeather={currentWeather} darkMode={darkMode} />
         </div>
+
     );
 }
 
